fc69d0e0233d147eac2e176505630bde4551fef2
WashiraSuyajai <washira.suyajai@gmail.com>
2020-03-11 06:20:43 +0700

    Update chatroom and server to count action

--- chat-server/server.js
+++ chat-server/server.js
@@ -1,9 +1,13 @@
 const server = require('http').createServer();
 const io = require('socket.io')(server);
 
+//let count = 0;
+
 io.on("connection", function(socket) {
-    socket.on("emit", function() {
-        console.log("receivve data")
+    socket.on("emit", function(data) { //1 fill argument data
+        console.log(data) //2 fill data to print
+        socket.broadcast.emit("count", { count : data.count }) 
+        //server ทำการ emit ค่า count ส่งไปหา client ที่เหลือ ด้วย Broadcast
     });
 });
 
@@ -12,3 +16,5 @@ server.listen(8080, function(err) {
     if (err) throw err;
     console.log('Server is listening to port 8080');
 });
+
+//ทุกๆการอัพเดท source code ฝั่ง server จะต้องทำการรัน server ใหม่ ไม่เหมือน client ที่รันเองอัตโนมัติ
--- react-sample/src/chat/Chatroom.js
+++ react-sample/src/chat/Chatroom.js
@@ -3,20 +3,37 @@ import socketIOClient from 'socket.io-client';
 
 class Chatroom extends Component {
     state = {
-        socket : null
+        socket : null,
+        count : 0 //เพิ่ม count และ initial ค่า 0
     };
 
     componentDidMount() {
         const socket = socketIOClient('localhost:8080');
 
         this.setState({ socket: socket });
+        
+        socket.on("count", data => this.handleCount(data))
 
-        socket.emit("emit", { data: "test" });
+        //socket.emit("emit", { data: "test" }); 
+        //จะส่ง "emit" ไปหา server จากนั้น server จะทำการรันกับ { data: "test" }
     }
 
+    handleCount = data => {
+        this.setState({ count : data.count })
+    }
+
+    onclick = () => {
+        let { socket, count } = this.state; //spread operator
+        count = count +1;
+        this.setState({ count : count });
+        socket.emit("emit", { count : count });
+    }
     render() {
-        return <div>Chatroom</div>
-    }
+        return <div>
+            <div>{this.state.count}</div>
+            <button onClick={this.onclick}>Count</button>
+        </div>
+    } //เพิมปุ่ม "Count" เมื่อกด จะรัน function "onClick"
 }
 
 export default Chatroom;
