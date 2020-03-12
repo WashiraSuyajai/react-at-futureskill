f44d0e09d76be19e099a295caf36afa5327f6ac8
Washira <us_pou@hotmail.com>
2020-03-13 06:18:49 +0700

    Add socket.on at server

--- chat-server/server.js
+++ chat-server/server.js
@@ -4,11 +4,14 @@ const io = require('socket.io')(server);
 //let count = 0;
 
 io.on("connection", function(socket) {
-    socket.on("emit", function(data) { //1 fill argument data
+    socket.on("emit", function(data) { //1 fill argument 'data'
         console.log(data) //2 fill data to print
         socket.broadcast.emit("count", { count : data.count }) 
         //server ทำการ emit ค่า count ส่งไปหา client ที่เหลือ ด้วย Broadcast
-    });
+    })
+    socket.on("emitChat", function(data) {
+        socket.broadcast.emit("message", { ...data });
+    })
 });
 
 
