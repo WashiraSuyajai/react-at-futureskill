bee8b89c103d917248bfd2cacf7405daa9e6591b
Washira <us_pou@hotmail.com>
2020-03-15 21:38:50 +0700

    Import navbar and change old link to navbar link

--- react-sample/src/App.js
+++ react-sample/src/App.js
@@ -7,6 +7,17 @@ import Request from './lib/Request';
 import Count from './chat/Count';
 import Chatroom from './chat/Chatroom';
 import Chatform from './chat/Chatform';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  NavbarText
+} from 'reactstrap';
+
 class App extends Component {
 
   state = {
@@ -22,6 +33,32 @@ class App extends Component {
     const {name} = this.state
     return (
       <div> 
+
+        <Navbar color="light" light expand="md">
+          
+          
+            <Nav className="mr-auto" navbar>
+            <NavItem>
+                <NavLink href="/">Home</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="/users">Users</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="/posts">Posts</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="/count">Count</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="/chat">Chat</NavLink>
+              </NavItem>
+            </Nav>
+            <NavbarText>Simple Text</NavbarText>
+          
+        </Navbar>
+
+        {/* Link แบบเดิม 
         <div>
           Hello {name}
         </div>
@@ -32,7 +69,7 @@ class App extends Component {
           <Link to="/count">Count</Link>
           <Link to="/chat">Chat</Link>
         </div>
-        
+        */}  
         <Route 
           path="/users" 
           component={() => (
